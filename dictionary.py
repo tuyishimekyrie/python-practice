8447f3e7771b2bec8158a9347367408ba8e970d7
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-01-09 04:43:10 +0200

    add dict comprehensions

--- dictionary.py
+++ dictionary.py
@@ -2,3 +2,11 @@ point = {"x":1,"y":2};
 data = dict(x=1,y=2);
 print(point);
 print(data);
+
+print(data.get("x"))
+
+for item,v in data.items():
+    print(item,v)
+    
+numbers = {item:data[item] for item in data}
+print(numbers)
